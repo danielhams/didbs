13dd412ec276db6f6e05604ccbf91c16163a1649
Daniel Hams <daniel.hams@gmail.com>
2019-10-23 22:49:59 +0100

    Use more reasonable levels of optimisation

--- packages/libpopt/setupenv.sh
+++ packages/libpopt/setupenv.sh
@@ -7,8 +7,8 @@ CPPFLAGS="-I$INSTALLDIR/include"
 CC=gcc
 CXX=g++
 # Use libdicl for better printf format string parsing.
-CFLAGS="$DIDBS_ISA_SWITCH -g -Og -I$INSTALLDIR/include -I$INSTALLDIR/include/libdicl-0.1"
-#CFLAGS="$DIDBS_ISA_SWITCH $DIDBS_GCC_OPT_SWITCH -I$INSTALLDIR/include -I$INSTALLDIR/include/libdicl-0.1"
+CFLAGS="$DIDBS_ISA_SWITCH $DIDBS_GCC_OPT_SWITCH -I$INSTALLDIR/include -I$INSTALLDIR/include/libdicl-0.1"
+#CFLAGS="$DIDBS_ISA_SWITCH -g -O2 -I$INSTALLDIR/include -I$INSTALLDIR/include/libdicl-0.1"
 LDFLAGS="$DIDBS_ISA_SWITCH -L$INSTALLDIR/$DIDBS_LIBDIR -ldicl-0.1"
 
 CXXFLAGS="$CFLAGS"
