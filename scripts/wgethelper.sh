b73071dda9a43c7158c7584c1f3234998abbf93a
Daniel Hams <daniel.hams@gmail.com>
2019-11-06 15:18:34 +0000

    courtesy thor@discord - use curl instead of wget which is broken in 0.1.7

--- scripts/wgethelper.sh
+++ scripts/wgethelper.sh
@@ -10,6 +10,10 @@ cd $DESTDIR || exit -1;
 # We can use the "/usr/didbs/current/bin/wget" now,
 # which includes recent cacerts, so now it will properly check.
 # No more statically linked wget skipping certs.
-wget $SOURCEURL || exit -1;
+
+# wget currently broken in 0.1.7 - courtesy thor
+# this can be worked around by using curl
+#wget $SOURCEURL || exit -1;
+curl -s -L -O $SOURCEURL || exit -1;
 
 exit 0;
